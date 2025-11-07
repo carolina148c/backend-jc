ea888fca776d9c809dcb85821de66e394e850246
carolina148c <ac176600@gmail.com>
2025-11-06 19:43:36 -0500

    nuevos archivos en src

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 // Middleware para procesar JSON
 app.use(express.json());
+
+// Conexión a MongoDB
 
 // Ruta base de prueba
 app.get('/', (req, res) => {
@@ -10,5 +15,4 @@ app.get('/', (req, res) => {
 });
 
 // Puerto
-const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
